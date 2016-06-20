c1eb0b5dea62c053c6ea6c0832156670ce0b8021
Tynan Stack <tynan.stack@gmail.com>
2016-06-20 12:43:27 -0700

    edit

--- server.js
+++ server.js
@@ -19,8 +19,19 @@ app.get('/', function(request, response){
 app.get('/todos',function(request,response){
 	response.json(todos);
 });
-app.get('/todos/:id',function(request,response){
-	response.send('asking for todo with id of %d',request.params.id);
+app.get('/todos/:id',function(req,res){
+	var todoid = req.params.id;
+	//iterate over todo's aray to find a match
+	var matchedToDo;
+	for (i=0;i<todos.length;i++){
+		if (todoid == todos[i].id){
+			matchedToDo = todos[i]
+		}
+	}
+	if (matchedToDo){
+		res.json(matchedToDo);
+	}else{
+	response.status(404).send();}
 });
 app.listen(PORT,function(){
 	console.log('express listening on port %d',PORT);
